a3b29a029d685318d4238a9624c68be23b0b2068
AmberKramer <amber.kramer@student.hu.nl>
2018-09-13 10:41:05 +0200

    les4

--- les 4/3.InputOutput.py
+++ les 4/3.InputOutput.py
@@ -1,4 +1,4 @@
 loon=input('How much do you earn?:')
 uren=input('How long did you work?:')
 winst=(float(loon)*float(uren))
-print('If you have worked for '+str(uren)+" hours, you wouldv'e earned €"+str(winst))
+print('If you worked for '+str(uren)+" hours, you wouldv'e earned €"+str(winst))
