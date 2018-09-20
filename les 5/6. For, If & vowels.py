d9bec6394798a2187ab3f0f515a3a35c557024c3
AmberKramer <amber.kramer@student.hu.nl>
2018-09-20 08:36:21 +0200

    les5 verbetering

--- les 5/6. For, If & vowels.py
+++ les 5/6. For, If & vowels.py
@@ -1,4 +1,10 @@
 s="Guido van Rossum heeft programmeertaal Python bedacht."
 for char in s:
+    if char in "aeiuo":
+        print(char)
+
+"old"
+s="Guido van Rossum heeft programmeertaal Python bedacht."
+for char in s:
     if char=="a" or char=="e" or char=="i" or char=="u" or char=="o":
         print(char)
