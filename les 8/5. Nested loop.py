746630e66f7826d4f799f34b7b4a31a502b47ee8
AmberKramer <amber.kramer@student.hu.nl>
2018-10-05 21:25:30 +0200

    les6

--- les 8/5. Nested loop.py
+++ les 8/5. Nested loop.py
@@ -1,8 +1,5 @@
-list=[1,2,3,4,5,6,7,8,9,10] +
-number2=0
-for number1 in list:
-    number1=number2
-    for number in number2:
-        print(number1+"*"+number2+"="+(number1*number2))
+getallen=[1,2,3,4,5,6,7,8,9,10]
 
-        
+for i in getallen:
+    for getal in getallen:
+        print("{:2} x {:2} = {:3}".format (i,getal, i*getal))
