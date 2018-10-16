e9aa92d5beed1a966da2ada32b54b468ef967855
AmberKramer <amber.kramer@student.hu.nl>
2018-10-16 10:33:21 +0200

    les6

--- les 8/2. Lists & strings.py
+++ les 8/2. Lists & strings.py
@@ -1,1 +1,12 @@
-#Deze file is per ongluk al mee gekomen... Deze code komt er nog aan.
+list=(input("Geef lijst met minimaal 10 strings: "))
+outfile = open("strings.txt", "w")
+outfile.write(list)
+outfile.close()
+infile=open("strings.txt",'r')
+regel=infile.readlines()
+for words in regel:
+    woord=words.split("\", \"")
+    print(woord)
+    for letter in woord:
+        if len(letter) == 4:
+            print(letter)
